bc85085263fefa4947d3cec2e0edc2d2e9069051
Jordan Haynes <jwhaynes101@gmail.com>
2024-08-16 18:59:17 -0400

    Updated AccountProfile functionality to allow users to modify their bio.

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -18,6 +18,8 @@ import { UserValidation } from '@/lib/validations/user';
 import { z } from 'zod';
 import Image from 'next/image';
 import { ChangeEvent, useState } from 'react';
+import { isBase64Image } from '@/lib/utils';
+import { useUploadThing } from '@/lib/uploadthing';
 
 interface Props {
     user: {
@@ -33,6 +35,7 @@ interface Props {
 
 const AccountProfile = ({ user, btnTitle }: Props) => {
     const [files, setFiles] = useState<File[]>([])
+    const { startUpload } = useUploadThing("media");
 
     const form = useForm({
         resolver: zodResolver(UserValidation),
@@ -49,7 +52,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
 
         const fileReader = new FileReader();
 
-        if(e.target.files && e.target.files.length > 1) {
+        if(e.target.files && e.target.files.length > 0) {
             const file = e.target.files[0];
 
             setFiles(Array.from(e.target.files));
@@ -57,15 +60,29 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
             if(!file.type.includes('image')) return;
 
             fileReader.onload = async (event) => {
-                const imageDataUrl = event.target?.result;
+                const imageDataUrl = event.target?.result?.toString() || '';
+
+                fieldChange(imageDataUrl);
             }
+
+            fileReader.readAsDataURL(file);
         }
     }
 
-    function onSubmit(values: z.infer<typeof UserValidation>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values)
+    const onSubmit = async (values: z.infer<typeof UserValidation>) => {
+        const blob = values.profile_photo;
+
+        const hasImageChanged = isBase64Image(blob);
+
+        if (hasImageChanged) {
+            const imgRes = await startUpload(files)
+
+            if (imgRes && imgRes[0].fileUrl) {
+                values.profile_photo = imgRes[0].fileUrl;
+            }
+        }
+
+        // TODO: Update user profile
       }
 
     return (
